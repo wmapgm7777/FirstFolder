4f8a123fdce6488f1ec3fd37a7bc79d8c5473462
wchen <wchen@fpcusa.com>
2021-01-31 21:26:51 -0500

    update

---
+++ appevent.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on('messageLogged',function(){
+    console.log('Listener called');
+})
+
+// Raise an event
+emitter.emit('messageLogged');
+// Making a noise, produce -
